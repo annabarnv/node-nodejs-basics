393120a206ab2447ab80b2c5290c452a0ab28b6c
Anna Baranova <anna.barnv@gmail.com>
2025-10-26 23:06:35 +0100

    feat: add modules and hash implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-  // Write your code here
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+
+        const readStream = createReadStream(filePath);
+
+        const hash = createHash('sha256');
+
+        await pipeline(
+            readStream,
+            hash
+        );
+
+        const hexHash = hash.digest('hex');
+
+        console.log(hexHash);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();
